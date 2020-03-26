c294ba6b3ef41e52381077c8ad5c422c1ffecdde
Ganesh Agrawal <ntplinterns@gmail.com>
2020-03-26 19:47:27 +0530

    Ganesh Agrawal | 3 Comp UI & Style Changes and Fixing

--- exam-handler/src/components/Login.js
+++ exam-handler/src/components/Login.js
@@ -1,52 +1,83 @@
 import React from 'react';
-import './u12.css';
+import './checkbox.container.css';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-//FIXME: UI needs full rework
+// ALL UI CHANGES FIXED || 26 March 2020 || Ganesh Agrawal
 
 class Login extends React.Component{
-    handleClick = () => {
-        var x = document.getElementById("U12mypass");
-        if (x.type==="password"){
-            x.type="text"
-        }
-        else{
-            x.type="password";
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             password: '',
+             username: ''
         }
     }
+
+    handleInputChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+    handleSignIn = (e) => {
+        // revoke default behaviour of submit button
+        e.preventDefault()
+        // disable button & Inputs
+        this.signInButtonRef.disabled = true
+        this.usernameInputRef.disabled = true
+        this.passwordInputRef.disabled = true
+        this.passwordInputRef.type = 'password'
+        this.signInButtonRef.disabled = true
+        // Main Login Here
+        //
+        //
+        // enable button and inputs
+        // this.signInButtonRef.disabled = false
+        // this.usernameInputRef.disabled = false
+        // this.passwordInputRef.disabled = false
+        // this.signInButtonRef.disabled = false
+    }
+
     render(){
         return (
-            <div className="container h-100" style={{position:"absolute"}}>
-                <div className="row w-100">
-                    <div className="col-md-6">
-                        <div className="container" style={{position:"relative",left:"40%",transform:'translateY(-50%)'}}>
-                            <img src="/assets/images/u12.png" alt="pic" width="400px" height="200px" />
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="container" style={{position:"relative",left:"30%", transform:'translateY(-50%)'}}>  
-                        <h5 style={{color:"blue"}}>OES ></h5>
-                        <p className="text-muted" style={{marginTop:"-10px"}}>Admin Login</p>
-                        <br />
-                        <h2 style={{fontFamily:"sans-serif"}}>Welcome!</h2>
-                        <p className="text-muted" style={{marginTop:"-12px"}}>Enter your credentials for Login !</p>
-                        <div>
-                            <form className="form-label">
-                                Username
-                                <input type="text" id="U12myname" className="form-control" name="UserName" required />
-                                Password
-                                <input type="password" className="form-control" id="U12mypass" name="Password" required />
-                                <div><input type="checkbox" class="checkmark" onClick={this.handleClick}/> &nbsp;<span style={{color:"#007bff"}}>Show Password</span></div>
-                                <br />
-                                <div>
-                                <span><a href="www.google.com">Forgot Password?</a></span>&emsp;&emsp;&emsp;&emsp;<span><button type="sumbit"
-                                 class="btn btn-primary" id="U12btn">Sign In</button></span>
-                                </div>
-                            </form>
-                        </div>
-                        </div> 
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <Container fluid={true} style={{height: '100vh',overflow: 'hidden'}}>
+                    <Row>
+                        <Col md={{span:3, offset:2}} style={{height: '100vh', position: "relative"}}>
+                            <Container fluid={true} style={{position: "absolute", top: '50%', transform: 'translateY(-50%)'}}>
+                                <img src="/assets/images/u12.png" alt="AssetImage" style={{width:'100%'}} />
+                            </Container>
+                        </Col>
+                        <Col md={{span:3, offset:2}} style={{height: '100vh', position: "relative"}}>
+                            <Container fluid={true} style={{position: "absolute", top: '50%', transform: 'translateY(-50%)'}}>
+                                <h5 style={{color:'#2196F3', marginBottom:'2px'}}>OES ></h5>
+                                <p className="text-muted">Admin Login</p>
+                                <h2 style={{fontFamily:"sans-serif", marginBottom:'2px'}}>Welcome!</h2>
+                                <p className="text-muted">Enter your credentials for Login!</p>
+                                <Form>
+                                    <Form.Label className="text-muted" style={{fontSize:'0.8rem', marginBottom:'0.1rem'}}>Username</Form.Label>
+                                    <Form.Control size="sm" type="text" name="username" placeholder="Enter username" onChange={this.handleInputChange} ref={input => this.usernameInputRef=input} style={{border:'none', borderBottom:'1px solid black', outline: 'none', boxShadow: 'none', borderRadius: '0px', marginBottom: '0.4rem'}} />
+                                    <Form.Label className="text-muted" style={{fontSize:'0.8rem',marginBottom:'0.1rem'}}>Password</Form.Label>
+                                    <Form.Control size="sm" type="password" name="password" placeholder="Enter password" onChange={this.handleInputChange} ref={input => this.passwordInputRef=input} style={{border:'none', borderBottom:'1px solid black', outline: 'none', boxShadow: 'none', borderRadius: '0px', marginBottom: '0.4rem'}} />
+                                    <Row style={{marginBottom:'1rem'}}>
+                                        <Col md={6}>
+                                            <Form.Check type="checkbox" id="show_password" className="checkbox-container">
+                                                <Form.Check.Input type="checkbox" onChange={(e)=> (this.passwordInputRef.type = e.target.checked ? 'text' : 'password')} ref={ref => this.checkBoxRef=ref} />
+                                                <span className="checkmark" onClick={(e) => this.checkBoxRef.click()}></span>
+                                                <Form.Check.Label style={{color:'#2196F3'}}>Show Password</Form.Check.Label>
+                                            </Form.Check>
+                                        </Col>
+                                        <Col md={6}>
+                                            <a href="#forgotPassword" alt="Forgot Password?" style={{float:'right', fontSize: '0.8rem',color:'#2196F3'}}>Forgot Password?</a>
+                                        </Col>
+                                    </Row>
+                                    <Button variant="primary" type="submit" size="sm" style={{float:'right', borderRadius:'1rem', padding:'.25rem 1.5rem', backgroundColor:'#2196F3', borderColor:'#2196F3'}} ref={ref => this.signInButtonRef=ref} onClick={this.handleSignIn} >Sign in</Button>
+                                </Form>
+                            </Container>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         )
     }
 }
--- exam-handler/src/components/QPAlreadyDecrypted.js
+++ exam-handler/src/components/QPAlreadyDecrypted.js
@@ -1,58 +1,152 @@
 import React from 'react';
-import './u2.css';
+import './toggle.css';
+import { Container, Row, Col, Media } from 'react-bootstrap';
 
-//FIXME: UI needs full rework
+// ALL UI CHANGES FIXED || 26 March 2020 || Ganesh Agrawal
 
 class QPAlreadyDecrypted extends React.Component{
+
+    componentDidMount(){
+        // mark lock checkbox as locked
+        this.lockRef.checked = true;
+    }
     
     render(){
         return (
-            <div className="container h-100" style={{position:"absolute"}}>
-                <div className="row w-100">
-                    <div className="col-md-6">
-                        <div className="container" style={{position:"relative",left:"20%",transform:'translateY(-50%)'}}>
-                            <h2>Decrypt Question Paper</h2>
-                            <p style={{marginTop:"-5px"}}>Decrypt the question paper by uploading its key's in its relevant set
-                            and by clicking the decrypt button below 
-                            </p>
-                            <img src="/assets/images/u2.jpg" alt="pic" width="500px" height="300px"/>
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="container" style={{position:"relative",left:"30%",transform:'translateY(-50%'}}>
-                        <label class="switch">
-                        <input type="checkbox" />
-                        <span class="slider round"></span>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Ei-lock.svg/512px-Ei-lock.svg.png" class="slider-img" />
-                        </label>
-                        <div class="media py-2 px-4" style={{backgroundColor:"#EDF4F2",borderRadius:"10px",width:"70%"}} >
-                        <div class="media-left media-middle">
-                        <p><span><img src="/assets/svg/padlock.svg" class="media-object" style={{width:"20px"}} /></span>
-                        &nbsp;&nbsp;<b>Set 1</b>&emsp;&emsp;<span>Paper Set Name 1</span>&emsp;&emsp;<img src='/assets/svg/check-mark.svg' class="media-object" 
-                        style={{width:"20px"}} /></p>
-                        <hr />
-                        <p><span><img src="/assets/svg/padlock.svg" class="media-object" style={{width:"20px"}} /></span>
-                        &nbsp;&nbsp;<b>Set 2</b>&emsp;&emsp;<span>Paper Set Name 2</span>&emsp;&emsp;<img src='/assets/svg/check-mark.svg' class="media-object" 
-                        style={{width:"20px"}} /></p>
-                        <hr />
-                        <p><span><img src="/assets/svg/padlock.svg" class="media-object" style={{width:"20px"}} /></span>
-                        &nbsp;&nbsp;<b>Set 3</b>&emsp;&emsp;<span>Paper Set Name 3</span>&emsp;&emsp;<img src='/assets/svg/check-mark.svg' class="media-object" 
-                        style={{width:"20px"}} /></p>
-                        <hr />
-                        <p><span><img src="/assets/svg/padlock.svg" class="media-object" style={{width:"20px"}} /></span>
-                        &nbsp;&nbsp;<b>Set 4</b>&emsp;&emsp;<span>Paper Set Name 4</span>&emsp;&emsp;<img src='/assets/svg/check-mark.svg' class="media-object" 
-                        style={{width:"20px"}} /></p>
-                        </div>
-                        </div>
-                        <div>
-                            <br />
-                        <h4><img src="/assets/svg/green.svg" style={{width:"20px"}} /> &nbsp; Decryption Successful</h4>
-                        <p>The current set of question paper is already decrypted</p> 
-                        </div>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <Container fluid={true} style={{height: '100vh',overflow: 'hidden'}}>
+                    <Row>
+                        <Col md={{span:4, offset:1}} style={{height: '100vh', position: "relative"}}>
+                            <Container fluid={true} style={{position: "absolute", top: '50%', transform: 'translateY(-50%)'}}>
+                                <h4>Decrypt Question Paper</h4>
+                                <p>Decrypt the Question paper by Uplodaing it's Key in its Relevent Set and by Clicking the Decrypt button below.</p>
+                                <img src="/assets/images/admin-panel-sidebar.jpg" alt="CheckList" style={{width:'100%'}} />
+                            </Container>
+                        </Col>
+                        <Col md={{span:4,offset:1}} style={{height: '100vh', position: "relative"}}>
+                            <Container fluid={true} style={{position: "absolute", top: '50%', transform: 'translateY(-50%)'}}>
+                                <Container fluid={true} style={{position: "absolute", left:'100%', top:'-50px'}}>
+                                    <label className="switch">
+                                        <input type="checkbox" id="padlock" ref={ref => this.lockRef=ref} />
+                                        <span className="slider round"></span>
+                                        <img src="/assets/svg/padlock.svg" alt="L" className="slider-img" />
+                                    </label>
+                                </Container>
+                                <Container fluid={true} style={{backgroundColor:'#f9f9f9', borderRadius:'1rem', marginBottom:'2rem'}} className="px-5 py-4">
+                                    <Media className="mb-2">
+                                        <img className="align-self-center mr-3" src="/assets/svg/document-security-unlock.svg" width={'25px'}  alt="Lock" />
+                                        <Media.Body className="border-bottom pb-2">
+                                            <table>
+                                                <tbody>
+                                                    <tr>
+                                                        <th className="pr-3">Set 1</th>
+                                                        <td>Paper Set Name 1</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </Media.Body>
+                                        <img className="align-self-center mr-3" src="/assets/images/Asset 8@4x.png" style={{width:'1.25rem',marginLeft:'1rem'}}  alt="green checkmark" />
+                                    </Media>
+                                    <Media className="mb-2">
+                                        <img className="align-self-center mr-3" src="/assets/svg/document-security-unlock.svg" width={'25px'}  alt="Lock" />
+                                        <Media.Body className="border-bottom pb-2">
+                                            <table>
+                                                <tbody>
+                                                    <tr>
+                                                        <th className="pr-3">Set 2</th>
+                                                        <td>Paper Set Name 2</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </Media.Body>
+                                        <img className="align-self-center mr-3" src="/assets/images/Asset 8@4x.png" style={{width:'1.25rem',marginLeft:'1rem'}}  alt="green checkmark" />
+                                    </Media>
+                                    <Media className="mb-2">
+                                        <img className="align-self-center mr-3" src="/assets/svg/document-security-unlock.svg" width={'25px'}  alt="Lock" />
+                                        <Media.Body className="border-bottom pb-2">
+                                            <table>
+                                                <tbody>
+                                                    <tr>
+                                                        <th className="pr-3">Set 3</th>
+                                                        <td>Paper Set Name 3</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </Media.Body>
+                                        <img className="align-self-center mr-3" src="/assets/images/Asset 8@4x.png" style={{width:'1.25rem',marginLeft:'1rem'}}  alt="green checkmark" />
+                                    </Media>
+                                    <Media className="mb-2">
+                                        <img className="align-self-center mr-3" src="/assets/svg/document-security-unlock.svg" width={'25px'}  alt="Lock" />
+                                        <Media.Body className="border-bottom pb-2">
+                                            <table>
+                                                <tbody>
+                                                    <tr>
+                                                        <th className="pr-3">Set 4</th>
+                                                        <td>Paper Set Name 4</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </Media.Body>
+                                        <img className="align-self-center mr-3" src="/assets/images/Asset 8@4x.png" style={{width:'1.25rem',marginLeft:'1rem'}}  alt="green checkmark" />
+                                    </Media>
+                                </Container>
+                                <Container fluid={true} style={{textAlign:'center'}}>
+                                    <h4><img src="/assets/images/Asset 8@4x.png" style={{width:"1.5rem", marginRight:'1rem'}} alt="green checkmark" /><b>Decryption Successful</b></h4>
+                                    <p style={{fontSize:'1rem'}} className="text-muted">
+                                        The current set of question paper is already decrypted.
+                                    </p>
+                                </Container>
+                            </Container>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
+            // <div className="container h-100" style={{position:"absolute"}}>
+            //     <div className="row w-100">
+            //         <div className="col-md-6">
+            //             <div className="container" style={{position:"relative",left:"20%",transform:'translateY(-50%)'}}>
+            //                 <h2>Decrypt Question Paper</h2>
+            //                 <p style={{marginTop:"-5px"}}>Decrypt the question paper by uploading its key's in its relevant set
+            //                 and by clicking the decrypt button below 
+            //                 </p>
+            //                 <img src="/assets/images/u2.jpg" alt="pic" width="500px" height="300px"/>
+            //             </div>
+            //         </div>
+            //         <div className="col-md-6">
+            //             <div className="container" style={{position:"relative",left:"30%",transform:'translateY(-50%'}}>
+            //             <label class="switch">
+            //             <input type="checkbox" />
+            //             <span class="slider round"></span>
+            //             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Ei-lock.svg/512px-Ei-lock.svg.png" class="slider-img" />
+            //             </label>
+            //             <div class="media py-2 px-4" style={{backgroundColor:"#EDF4F2",borderRadius:"10px",width:"70%"}} >
+            //             <div class="media-left media-middle">
+            //             <p><span><img src="/assets/svg/padlock.svg" class="media-object" style={{width:"20px"}} /></span>
+            //             &nbsp;&nbsp;<b>Set 1</b>&emsp;&emsp;<span>Paper Set Name 1</span>&emsp;&emsp;<img src='/assets/svg/check-mark.svg' class="media-object" 
+            //             style={{width:"20px"}} /></p>
+            //             <hr />
+            //             <p><span><img src="/assets/svg/padlock.svg" class="media-object" style={{width:"20px"}} /></span>
+            //             &nbsp;&nbsp;<b>Set 2</b>&emsp;&emsp;<span>Paper Set Name 2</span>&emsp;&emsp;<img src='/assets/svg/check-mark.svg' class="media-object" 
+            //             style={{width:"20px"}} /></p>
+            //             <hr />
+            //             <p><span><img src="/assets/svg/padlock.svg" class="media-object" style={{width:"20px"}} /></span>
+            //             &nbsp;&nbsp;<b>Set 3</b>&emsp;&emsp;<span>Paper Set Name 3</span>&emsp;&emsp;<img src='/assets/svg/check-mark.svg' class="media-object" 
+            //             style={{width:"20px"}} /></p>
+            //             <hr />
+            //             <p><span><img src="/assets/svg/padlock.svg" class="media-object" style={{width:"20px"}} /></span>
+            //             &nbsp;&nbsp;<b>Set 4</b>&emsp;&emsp;<span>Paper Set Name 4</span>&emsp;&emsp;<img src='/assets/svg/check-mark.svg' class="media-object" 
+            //             style={{width:"20px"}} /></p>
+            //             </div>
+            //             </div>
+            //             <div>
+            //                 <br />
+            //             <h4><img src="/assets/svg/green.svg" style={{width:"20px"}} /> &nbsp; Decryption Successful</h4>
+            //             <p>The current set of question paper is already decrypted</p> 
+            //             </div>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
         )
     }
 }
