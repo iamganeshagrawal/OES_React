ead69ca64907d5a6692cf4c026c2a0c60af6e55c
Ganesh Agrawal <gmegotiya@gmail.com>
2020-04-22 17:49:05 +0530

    EndExam || Response Sheet UI Fixed and empty func bounded

--- exam-admin/src/App.js
+++ exam-admin/src/App.js
@@ -3,17 +3,17 @@ import { Provider } from 'react-redux';
 import store from './config/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Loader from './components/FullPageLoader';
-// import Router from './config/appRoutes';
+import Router from './config/appRoutes';
 import { ToastContainer } from 'react-toastify';
 
-import Page from './components/centerDash';
+// import Page from './components/EndExam';
 function App() {
   return (
     <Provider store={store}>
       <Loader isLoading={true} />
 	  <ToastContainer />
-      {/* <Router /> */}
-      <Page />
+      <Router />
+      {/* <Page /> */}
     </Provider>
   );
 }
--- exam-admin/src/components/EndExam.js
+++ exam-admin/src/components/EndExam.js
@@ -1,36 +1,95 @@
 import React from 'react';
 import './css/EndExam.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import { InputGroupAddon,InputGroup,Input,Button} from 'reactstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+// 22 APRIL 2020 || All UI Changes Fixed || Ganesh Agrawal
 
 class EndExam extends React.Component{
-    constructor(props){
-        super(props);
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             selectedResponseSheet: 'placeholder',
+             avaliableResponseSheets : [],
+             currentExamStatus: 'ongoing', // 'ongoing', 'ended'
+        }
+    }
+    responseSheetSelectorHandler = (event) => {
+        this.setState({
+            selectedResponseSheet: event.target.value
+        })
+    }
+    handleGenerate = () => {
+        // Implement a filter that filter out selected res sheet from all avaliable res sheets.
+        console.log('hitted');
+        // and after that request to server for genrate and download
+    }
+    handleEndExam = () => {
+        // Implement end exam button fun here and set this.state.currenExamStatus='ended' after successfully end exam.
+        this.setState({
+            currentExamStatus: 'ended'
+        })
+    }
+    getCurrLabel = () => {
+        if(this.state.currentExamStatus === 'ongoing'){
+            return <span className="ongoing">Active</span>
+        }else{
+            return <span className="ended">Ended</span>
+        }
+    }
+
+    componentDidMount(){
+        // inserting mock data for response sheets && if data struct change then update options maping in render function
+        let mockResSheets = [
+            {
+                name: 'EXAM 01 Section A',
+                code: '458_EXAM_01_Sec_A'
+            },
+            {
+                name: 'EXAM 01 Section B',
+                code: '458_EXAM_01_Sec_B'
+            }
+        ]
+
+        this.setState({
+            avaliableResponseSheets: mockResSheets,
+            currentExamStatus: 'ongoing'
+        })
     }
     render(){
+        const {avaliableResponseSheets : resSheets, currentExamStatus: currStatus, selectedResponseSheet: currResSheet} = this.state;
         return (
-            <Container type="fluid">
-                <Row>
-                    <Col style={{left:"30%"}}>
-                    <img src="../u5.png" alt="image" width="400px" height="400px" />
-                    </Col>
-                    <Col style={{right:"-40%"}}>
-                    <div>
-                    <button type="button" className="btn btn-outline-dark"><b>Exam</b>&emsp;&emsp;<span class="px-2" style={{color:"white",backgroundColor:"#4dff4d",borderRadius:"4px"}}>Active</span></button>
-                    </div>
-                    <br />
-                    <br />
-                    <Button variant="outline-secondary light" style={{width:"50%",backgroundColor:"white"}} ><b style={{color:"red"}}>End Exam</b>
-                    </Button>
-                    <br />
-                    <br />
-                    <InputGroup size="lg">
-                    <Input placeholder="Name of the Exam based Response Sheet" style={{fontSize:"15px",width:"300px",height:"50px"}} /><span className="EElabel">Response Sheets</span>
-                    <InputGroupAddon addonType="append" ><Button value="Generate" style={{fontSize:"15px",color:"white",backgroundColor:"black",fontWeight:"bold"}}>Generate</Button></InputGroupAddon>
-                    </InputGroup>
-                    </Col>
-                </Row>
-            </Container>
+            <div>
+                <Container fluid={true} style={{height: '100vh',overflow: 'hidden'}}>
+                    <Row>
+                        <Col md={{span:4, offset:1}} style={{height: '100vh', position: "relative"}}>
+                            <Container fluid={true} style={{position: "absolute", top: '50%', transform: 'translateY(-50%)'}}>
+                                <img src="./assets/svg/1.svg" alt="CheckList" style={{width:'100%'}} />
+                            </Container>
+                        </Col>
+                        <Col md={{span:4,offset:2}} style={{height: '100vh', position: "relative"}}>
+                            <div className="res-exam-status">
+                                Exam {this.getCurrLabel()}
+                            </div>
+                            <Container fluid={true} style={{position: "absolute", top: '50%', transform: 'translateY(-50%)'}}>
+                                <Button variant="outline-danger" className="end-exam-button" disabled={!(currStatus==='ongoing')} onClick={this.handleEndExam}>End Exam</Button>
+                                    <div className="fieldBox">
+                                        <span className="fieldTitle">Response Sheet</span>
+                                        <select className="fieldInput select" defaultValue={currResSheet} onChange={this.responseSheetSelectorHandler}>
+                                            <option value='placeholder' disabled>Name of the exam based response sheet</option>
+                                            {
+                                                resSheets.map((sheet,i) => (
+                                                    <option key={i} value={sheet.code}>{sheet.name}</option>
+                                                ))
+                                            }
+                                        </select>
+                                        <button className="fieldButton" onClick={this.handleGenerate}>Generate</button>
+                                    </div>
+                            </Container>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         )
     }
 }
