ba26da16875fd96acf129b3114730abeffcb176e
Deependra Karwani <netparam.deependra@gmail.com>
2020-05-02 00:53:33 +0530

    BlockBrowserFunctionalities

--- exam-candidate/src/App.js
+++ exam-candidate/src/App.js
@@ -6,12 +6,38 @@ import store from './config/store';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
-  return (
-	<Provider store={store}>
-		<ToastContainer />
-    	<Router />
-	</Provider>
-  );
+	window.addEventListener("popstate", () => {
+		history.go(1);
+	});
+	// Back Button
+
+	window.onbeforeunload = function() {
+		return false;
+	}
+	// Refresh
+
+	document.onkeydown = function() {
+		switch (event.keyCode){
+			case 116 : //F5 button
+				event.returnValue = false;
+				event.keyCode = 0;
+				return false;
+			case 82 : //R button
+				if (event.ctrlKey) { 
+				event.returnValue = false;
+				event.keyCode = 0;
+				return false;
+				}
+		}
+	}
+	// Block F5 and Ctrl+R
+
+	return (
+		<Provider store={store}>
+			<ToastContainer />
+			<Router />
+		</Provider>
+	);
 }
 
 export default App;
