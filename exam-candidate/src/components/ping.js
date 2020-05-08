7896b18e46ebd6e51f353ba57826777dee5f9caa
Ganesh Agrawal <gmegotiya@gmail.com>
2020-05-08 18:55:36 +0530

    PingUI
    Former-commit-id: 4f3950e04501fd12c8d0079a7735669b8222b873 [formerly d5e76a4d433c0fca7105a90e4b9fdc8bad7e5cfc] [formerly c95859e52bd46781f89a0eac982e59234635b5de [formerly 1ba3760a247762b9ff34b38369ec76550e664fbc]]
    Former-commit-id: 59cf464519e29a62fbbfe35295302209628fbaeb [formerly d5d6e650bd4237b67c3d02c262b40b4de43be9e9]
    Former-commit-id: 383fb05576c24a3e38e8c7ffacfd37730013a12d

--- exam-candidate/src/components/ping.js
+++ exam-candidate/src/components/ping.js
@@ -4,6 +4,8 @@ import { saveIP } from '../actions/sessionsActions';
 import { ping, setBase } from '../config/httpRoutes';
 import { ipRE } from '../config/RegEx';
 import { alertError, alertSuccess } from '../config/toaster';
+import { Container, Row, Col } from 'react-bootstrap';
+import styles from './Ping.module.css';
 
 class Ping extends Component {
 	constructor(props) {
@@ -24,15 +26,16 @@ class Ping extends Component {
 		}
 	}
 
-	ipChange = ({ target }) => {
-		this.setState({ip: target.value});
+	ipChange = ({ target : { value } }) => {
+		const rx = /^[0-9.]+$/;
+		if(value === '' || rx.test(value)){
+			this.setState({ip: value});
+		}
 	}
-
 	saveIP = (e) => {
 		e.preventDefault();
 
 		let { ip } = this.state;
-
 		if(!ipRE.test(ip)) {
 			return alertError("Please Enter a Valid IP");
 		}
@@ -48,8 +51,41 @@ class Ping extends Component {
 	}
 
 	render() {
+		const { ip } = this.state;
+		const isValidIp = ipRE.test(ip);
 		return (
-			<></>
+			<div>
+                <Container fluid={true} style={{height: '100vh',overflow: 'hidden'}}>
+                    <Row>
+                        <Col md={{span:4, offset:1}} style={{height: '100vh', position: "relative"}}>
+                            <Container fluid={true} style={{position: "absolute", top: '50%', transform: 'translateY(-50%)'}}>
+                                <img src="/assets/images/server.png" alt="Servers" style={{width:'100%'}} />
+                            </Container>
+                        </Col>
+                        <Col md={{span:4,offset:2}} style={{height: '100vh', position: "relative"}}>
+                            <Container fluid={true} style={{position: "absolute", top: '50%', transform: 'translateY(-50%)'}}>
+                                <Container fluid={true}>
+                                        <div className={styles.fieldBox} style={isValidIp ? {borderColor: "var(--success)"} : {}}>
+                                            <span className={styles.fieldTitle}>Enter Server IP Address</span>
+											<input name="ip" type="text" 
+												className={styles.fieldInput} 
+												value={ip}
+												onChange={this.ipChange}
+												placeholder="Enter the IP Address of the Server" 
+												style={isValidIp ? {color: "var(--success)"} : {}}
+											/>
+											<button className={styles.fieldButton} 
+												onClick={this.saveIP}
+												style={isValidIp ? {backgroundColor: "var(--success)", "--hoverColor": "#006400"} : {}} >
+												Enter
+											</button>
+                                        </div>
+                                </Container>
+                            </Container>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
 		);
 	}
 }
